59a5941c9dd5428eed931f06d0e61ac1193c7be5
dongnguyenduybang <dndb20011512@gmail.com>
2025-01-03 16:42:45 +0700

    fix: add messagesmodule, friendmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './modules/fakers/user/user.module';
 import { ChannelModule } from './modules/fakers/channel/channel.module';
 import { ConfigModule } from '@nestjs/config';
-
+import { MessagesModule } from './modules/fakers/message/message.module';
+import { FriendModule } from './modules/fakers/friend/friend.module';
 @Module({
     imports: [
         ConfigModule.forRoot({
@@ -10,7 +11,9 @@ import { ConfigModule } from '@nestjs/config';
             envFilePath: '.env',
         }),
         UserModule,
-        ChannelModule
+        ChannelModule,
+        MessagesModule,
+        FriendModule
 
     ],
 })
