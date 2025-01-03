9ca5be872bd321a4fe8102c905b5da013942a8c1
dongnguyenduybang <dndb20011512@gmail.com>
2025-01-03 16:42:25 +0700

    fix: add mock messages, friends

--- src/common/api-config.ts
+++ src/common/api-config.ts
@@ -5,6 +5,8 @@ export const getApiConfig = (configService: ConfigService) => {
     const mockEndpoints = {
         users: configService.get<string>('MOCK_USERS_ENDPOINT'),
         channels: configService.get<string>('MOCK_CHANNELS_ENDPOINT'),
+        messages: configService.get<string>('MOCK_MESSAGES_ENDPOINT'),
+        friends: configService.get<string>('MOCK_FRIENDS_ENDPOINT'),
     };
 
     return { baseUrl, mockEndpoints };
