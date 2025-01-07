744a11295a8ffbfbe2f697f6e15ad867ce199701
dongnguyenduybang <dndb20011512@gmail.com>
2025-01-07 10:14:17 +0700

    fix: delete incorrect or outdated file

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ export default {
     moduleFileExtensions: ['js', 'json', 'ts'],
     rootDir: '.',
     testEnvironment: 'node',
-    testRegex: '\\.spec\\.ts$',  // Tìm tất cả các file .spec.ts
+    testRegex: '\\.spec\\.ts$',
     transform: {
         '^.+\\.(t|j)s$': 'ts-jest',
     },
