827500ab25ab5e7593d474b1db11a9e7c7fb9c32
dongnguyenduybang <dndb20011512@gmail.com>
2025-01-10 16:00:58 +0700

    feat: package

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+
 export default {
     moduleFileExtensions: ['js', 'json', 'ts'],
     rootDir: '.',
@@ -6,4 +8,15 @@ export default {
     transform: {
         '^.+\\.(t|j)s$': 'ts-jest',
     },
+    // Nạp file .env trước khi chạy test
+    setupFiles: ['dotenv/config'],
+    moduleNameMapper: {
+        '#ansi-styles': 'ansi-styles', // Khắc phục lỗi ESM của chalk
+    },
+    globals: {
+        'ts-jest': {
+            useESM: true, // Kích hoạt ESM trong ts-jest
+        },
+    },
+    extensionsToTreatAsEsm: ['.ts', '.tsx'], // Xem các tệp TypeScript là ESM
 };
