dc867f3dcd09653caa129b0a3801246b5ec5fba1
dongnguyenduybang <dndb20011512@gmail.com>
2025-01-16 17:15:36 +0700

    fix: fix response api to GetMesage

--- src/test/mock_message/response.spec.ts
+++ src/test/mock_message/response.spec.ts
@@ -1,28 +1,59 @@
-// import axios from 'axios';
-// import { getMockChannel } from '../share-data';
+import axios from 'axios';
+import { getMockUserDev } from '../share-data';
 
-// describe('MockChannel Test Response', () => {
-//     let baseUrl, baseUrlTestResponse, baseCountryCode, channelId
+describe('MockChannel Test Response', () => {
+    let baseUrl, baseUrlTestResponse, baseCountryCode, token, baseUrlDev
 
-//     beforeAll(async () => {
+    beforeAll(async () => {
 
-//         baseUrl = process.env.API_BASE_URL + 'InternalFaker/MockMessages';
-//         baseCountryCode = process.env.HEADER_COUNTRY_CODE;
-//         baseUrlTestResponse = process.env.API_BASE_URL + 'MessageView/ListMessages';
+        baseUrl = process.env.API_BASE_URL_DEV + 'InternalFaker/MockMessages';
+        baseCountryCode = process.env.HEADER_COUNTRY_CODE;
 
-//         const dataMockChannel = await getMockChannel()
-//         channelId = dataMockChannel.channelId
-//     });
+        baseUrlDev = process.env.API_BASE_URL_DEV + 'Channel/CreateChannel'
+        baseUrlTestResponse = process.env.API_BASE_URL_DEV + 'MessageView/GetMessage';
 
-//     it('Get data from ListMessages by messageId', async () => {
-//         try {
+        const mockUserToken = await getMockUserDev()
+        token = mockUserToken.token
+    });
 
-//             const testMessageIdResponse = await axios.get(`${baseUrlTestResponse}?channelId=${channelId}&workspaceId=0`);
+    it('Get data from ListMessages by messageId', async () => {
+        try {
+            //create channel
+            const createChannel = await axios.post(baseUrlDev, { workspaceId: '0', name: 'TestMessage', userIds: [] },
+                { headers: { 'x-session-token': token } })
 
-//             expect(testMessageIdResponse.status).toBe(200);
-//         }
-//         catch { }
+            const getChannelId = createChannel.data.data.channel.channelId
+            const getChannelWorkspaceId = createChannel.data.data.channel.workspaceId
 
-//     });
+            //mock message
 
-// })
+            const mockMessage = await axios.post(baseUrl, { workspaceId: getChannelWorkspaceId, channelId: getChannelId, quantity: 1 })
+
+            const messageId = mockMessage.data.data[0]
+
+            //get message 
+
+            const getMessage = await axios.get(`${baseUrlTestResponse}?workspaceId=${getChannelWorkspaceId}&channelId=${getChannelId}&messageId=${messageId}`, {
+                headers: {
+                    'x-session-token': token
+                }
+            })
+
+            const dataGetMessage = getMessage.data
+
+            expect(dataGetMessage.ok).toBe(true)
+
+            expect(dataGetMessage.data).toBeDefined()
+
+            expect(getChannelId).toEqual(dataGetMessage.data.message.channelId)
+
+            expect(messageId).toEqual(dataGetMessage.data.message.messageId)
+
+        }
+        catch (error) {
+            console.log(error)
+        }
+
+    });
+
+})
